1340f2ea667da1ef55a24deb33af9965222c3557
muhammadadeelinfo <muhammad.adeel.info@gmail.com>
2020-08-03 10:05:21 +0200

    added sales form and changes buttons pattern

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React, { Component } from "react";
 import "./index.css";
 import { Button } from "react-bootstrap";
 
-import NavBar from "./NavBar";
-import CardApp from "./CardApp";
-import SearchboxApp from "./SearchboxApp";
+import NavBarApp from "./NavBarApp";
+import SearchAll from "./SearchAll";
+import PurchaseApp from "./PurchaseApp";
+import SaleApp from "./SaleApp";
 import {
   BrowserRouter as Router,
   Route,
@@ -17,22 +18,25 @@ export default class App extends Component {
   render() {
     return (
       <>
-        <NavBar />
+        <NavBarApp />
 
         <div className="mainContainer">
           <Router>
             <switch>
-              <Route path="/SearchboxApp" component={SearchboxApp} />
-              <Route exact path="/CardApp" component={CardApp} />
+              <Route exact path="/PurchaseApp" component={PurchaseApp} />
+              <Route exact path="/SearchAll" component={SearchAll} />
+              <Route exact path="/SaleApp" component={SaleApp} />
             </switch>
             <div className={"fixed-bottom"} style={{ textAlign: "center" }}>
-              <Link to="/SearchboxApp" className="btn btn-primary">
-                Search
+              <Link to="/PurchaseApp" className="btn btn-success">
+                Buy
               </Link>
-              <Link to="CardApp" className="btn btn-info">
+              <Link to="/SaleApp" className="btn btn-primary">
+                Sale
+              </Link>
+              <Link to="SearchAll" className="btn btn-secondary">
                 Search All
               </Link>
-              <Link className="btn btn-success">Chat</Link>
               <Link className="btn btn-info">Alerts</Link>
             </div>
           </Router>
